1fb5bd2381619fb15268526908ce8bf719e15dc3
chrisnatali <chris.natali@gmail.com>
2017-11-18 17:21:57 -0500

    add mortgage schedule method

--- lib/ledger_tools.rb
+++ lib/ledger_tools.rb
@@ -23,6 +23,23 @@ module LedgerTools
         end
       end
     end
+
+    def self.generate_mortgage_schedule(principal, annual_rate, num_years)
+      # derived from http://www.konfluence.org/geomser/geomser.html  
+      monthly_rate = annual_rate / 12.0
+      num_months = num_years * 12
+      principal_with_no_payments = principal*((1 + monthly_rate)**(num_months))
+      monthly_factor = ((1 + monthly_rate)**(num_months) - 1) / monthly_rate
+      monthly_amount = principal_with_no_payments / monthly_factor
+      gen = Enumerator.new do |y|
+        (1...num_months).each do |month|
+          interest_payment = principal * monthly_rate
+          principal_payment = monthly_amount - interest_payment
+          principal = principal - principal_payment
+          y << [month, interest_payment, principal_payment, principal]
+        end
+      end
+    end
   end
 end
 
