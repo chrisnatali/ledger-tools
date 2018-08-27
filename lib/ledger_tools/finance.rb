486c34f78139e5385721e071fa0dc8518d6f2c10
chrisnatali <chris.natali@gmail.com>
2018-08-26 20:50:25 -0400

    added interest solver and template for StandardAnnuity class

--- lib/ledger_tools/finance.rb
+++ lib/ledger_tools/finance.rb
@@ -1,4 +1,13 @@
-module LedgerTools::Finance
+module Finance
+
+  MIN_ACCURACY = 1e-6
+
+  # TODO:  Write StandardAnnuity where we can set any 3 of the 4 variables
+  class StandardAnnuity
+    def initialize(num_periods:, principal: nil, periodic_interest: nil, annuity_payment: nil)
+    end
+  end
+
   # Solves geometric series for the periodic payment for a mortgage
   #
   # A mortgage is like an annuity for the bank where they advance a loan
@@ -61,6 +70,7 @@ module LedgerTools::Finance
     # over 1 period
     if num_periods == 0
       return principal
+    end
 
     principal / self.annuity_pv_factor(interest, num_periods)
   end
@@ -92,7 +102,7 @@ module LedgerTools::Finance
   #
   # p*(1 + i)^n - a*s_0
   def self.annuity_principal_balance(principal, interest, payment, num_periods)
-    (p * (1 + interest)**num_periods) - payment * self.annuity_fv_factor(interest, num_periods)
+    (principal * (1 + interest)**num_periods) - payment * self.annuity_fv_factor(interest, num_periods)
   end
 
  
@@ -111,6 +121,7 @@ module LedgerTools::Finance
     # if interest is 0, the pv is just the amount * num_periods
     if interest == 0
       return num_periods
+    end
 
     # closed form for the geometric series
     (1 - (1 + interest)**(-num_periods)) / interest
@@ -132,8 +143,53 @@ module LedgerTools::Finance
     # if interest is 0, the pv is just the amount * num_periods
     if interest == 0
       return num_periods
+    end
 
     # closed form for the geometric series
     ((1 + interest)**(num_periods) - 1) / interest
   end
+
+  # Solve for the periodic interest of a given annuity
+  # given:  principal, payment and number of periods
+  def self.solve_annuity_for_interest(
+    principal,
+    annuity_payment,
+    num_periods,
+    &annuity_payment_method)
+
+    raise StandardError("Invalid parameters: principal #{principal}, annuity_payment #{annuity_payment}, num_periods #{num_periods}") if annuity_payment >= principal || num_periods < 0 || annuity_payment <= 0 || principal <= 0
+    # Interest range must be from 0 (no interest) to < 1.0 (each payment would equal the principal)
+    # One millionth accuracy
+    # Since the annuity_payment function is monotonic in interest, we can solve for
+    # the interest by binary search until we get an annuity_payment that's close
+    # enough to what we're looking for
+    min_interest = 0
+    max_interest = 1.0
+    current_interest = (min_interest + max_interest) / 2.0
+    current_payment = annuity_payment_method.call(principal, current_interest, num_periods)
+    while ((annuity_payment - current_payment).abs > MIN_ACCURACY) 
+      if current_payment > annuity_payment
+        max_interest = current_interest
+      else
+        min_interest = current_interest
+      end
+      current_interest = (min_interest + max_interest) / 2.0
+      current_payment = annuity_payment_method.call(principal, current_interest, num_periods)
+    end
+    current_interest
+  end
+end
+
+if $0 == __FILE__
+  principal = 800_000.0
+  annuity_payment = 4000.0
+  num_periods = 360
+  interest = Finance.solve_annuity_for_interest(principal, annuity_payment, num_periods) do |p, i, n|
+    Finance.annuity_payment(p, i, n)
+  end
+
+  annual_interest = interest * 12
+  p "principal: #{principal} payment: #{annuity_payment} num_periods: #{num_periods} interest: #{interest} annual interest: #{annual_interest}"
+  recalculated_payment = Finance.annuity_payment(principal, interest, num_periods)
+  p "recalculated payment: #{recalculated_payment}"
 end
